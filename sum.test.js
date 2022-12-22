204a0d52141b759a58f8df0011003c35666264cc
qurriahSam <mugirethe1@gmail.com>
2022-12-22 19:47:28 +0300

    test: tests sum function

---
+++ sum.test.js
@@ -0,0 +1,8 @@
+const sum = require("./sum");
+
+describe("description one", () => {
+  it("should add 1 + 2 to equal 3", () => {
+    const result = sum(1, 2);
+    expect(result).toBe(3);
+  });
+});
