3ebef2f7c9cb05066b4f332af8151e31283a488d
qurriahSam <mugirethe1@gmail.com>
2022-12-23 13:13:20 +0300

    ft: check falsey and truthy values

--- sum.test.js
+++ sum.test.js
@@ -5,4 +5,19 @@ describe("description one", () => {
     const result = sum(1, 2);
     expect(result).toBe(3);
   });
+  it("to match objects/arrays use isEqual", () => {
+    const obj = { name: "sam" };
+    expect(obj).toEqual({ name: "sam" });
+  });
+});
+
+describe("check truthy or falsey", () => {
+  it("null check", () => {
+    const n = null;
+    expect(n).toBeFalsy();
+  });
+  it("true check", () => {
+    const n = true;
+    expect(n).toBeTruthy();
+  });
 });
