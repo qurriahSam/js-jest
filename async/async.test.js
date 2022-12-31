245c7006323cd3f2c779e78d565495e2522a2ca6
qurriahSam <mugirethe1@gmail.com>
2022-12-31 20:37:34 +0300

    test: mock async get request

--- async/async.test.js
+++ async/async.test.js
@@ -1,8 +1,22 @@
+const axios = require("axios");
 const fetchData = require("./async");
 
 describe("asynchronous tests", () => {
   it("should return correct todo for get request", async () => {
     const todo = await fetchData(1);
     expect(todo.id).toBe(1);
+  });
+});
+
+describe("mock async test", () => {
+  it("should return correct todo for mock get request", async () => {
+    jest.spyOn(axios, "get").mockReturnValueOnce({
+      data: {
+        id: 1,
+        todo: "This is a mock todo",
+      },
+    });
+    const results = await fetchData(1);
+    expect(results.todo).toBe("This is a mock todo");
   });
 });
